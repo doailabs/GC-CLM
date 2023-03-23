3d72e93bf18461c07dae3fd150f431de101becfa
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-23 21:55:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,88 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMContentLoaded');
+
     const platformClient = require('platformClient');
+    console.log('platformClient', platformClient);
+
     const clientApp = require('purecloud-client-app-sdk');
+    console.log('clientApp', clientApp);
 
     const client = platformClient.ApiClient.instance;
+    console.log('client', client);
+
     clientApp.init(client);
 
     const clientId = '479255ae-cf23-4c2e-9209-555370df882c';
     const redirectUri = `${window.location.protocol}//${window.location.host}`;
+    console.log('clientId', clientId);
+    console.log('redirectUri', redirectUri);
 
-    client.setEnvironment('mypurecloud.de');
+    client.setEnvironment('mypurecloud.com');
     client.setPersistSettings(true, 'GenesysCloudApp');
 
-    const downloadListsButton = document.getElementById('downloadListsButton');
-    downloadListsButton.addEventListener('click', downloadContactLists);
-
-    const contactListsTableBody = document.getElementById('contactListsTable').getElementsByTagName('tbody')[0];
-
     function displayContactLists(contactLists) {
-        contactListsTableBody.innerHTML = '';
+        console.log('displayContactLists', contactLists);
+        const contactListsContainer = document.getElementById('contactLists');
 
         contactLists.forEach(list => {
-            const row = document.createElement('tr');
-
-            const idCell = document.createElement('td');
-            idCell.textContent = list.id;
-            row.appendChild(idCell);
-
-            const nameCell = document.createElement('td');
-            nameCell.textContent = list.name;
-            row.appendChild(nameCell);
-
-            const createdByCell = document.createElement('td');
-            createdByCell.textContent = list.createdBy.name;
-            row.appendChild(createdByCell);
-
-            const createdDateCell = document.createElement('td');
-            createdDateCell.textContent = new Date(list.dateCreated).toLocaleDateString();
-            row.appendChild(createdDateCell);
-
-            const modifiedDateCell = document.createElement('td');
-            modifiedDateCell.textContent = new Date(list.dateModified).toLocaleDateString();
-            row.appendChild(modifiedDateCell);
-
-            contactListsTableBody.appendChild(row);
+            const listItem = document.createElement('p');
+            listItem.textContent = `${list.name} - ${list.id}`;
+            contactListsContainer.appendChild(listItem);
         });
     }
 
-    function downloadContactLists() {
+    function fetchContactLists() {
+        console.log('fetchContactLists');
         const apiInstance = new platformClient.OutboundApi();
 
         apiInstance.getOutboundContactlists()
             .then(response => {
+                console.log('getOutboundContactlists response', response);
                 const contactLists = response.entities;
-                downloadCsv(contactLists);
+                displayContactLists(contactLists);
             })
-            .catch(error => console.error('Error al descargar las contact lists:', error));
+            .catch(error => console.error('Error al cargar las contact lists:', error));
     }
 
-    function downloadCsv(contactLists) {
-        const csvContent = "data:text/csv;charset=utf-8,"
-            + "ID,Nombre,Creado por,Fecha de Creación,Fecha de Modificación\n";
-
-        contactLists.forEach(list => {
-            csvContent += `${list.id},"${list.name}",${list.createdBy.name},${new Date(list.dateCreated).toLocaleDateString()},${new Date(list.dateModified).toLocaleDateString()}\n`;
-        });
-
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "contactLists.csv");
-        document.body.appendChild(link);
-        link.click();
-    }
-
+    console.log('before loginImplicitGrant');
     client.loginImplicitGrant(clientId, redirectUri)
         .then(() => {
-            const apiInstance = new platformClient.OutboundApi();
-
-            apiInstance.getOutboundContactlists()
-                .then(response => {
-                    const contactLists = response.entities;
-                    displayContactLists(contactLists);
-                })
-                .catch(error => console.error('Error al cargar las contact lists:', error));
+            console.log('loginImplicitGrant success');
+            fetchContactLists();
         })
         .catch(error => console.error('Error al autenticar:', error));
 });
