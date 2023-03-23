61452c52f37d6824cc00b3210a5f3086f06fb681
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-23 23:17:14 +0100

    Update getContactLists.js

--- getContactLists.js
+++ getContactLists.js
@@ -1,5 +1,24 @@
+function fetchContactLists(platformClient) {
+    console.log('Inicio de fetchContactLists'); // Agregado
+
+    const outboundApi = new platformClient.OutboundApi();
+
+    outboundApi.getOutboundContactlists()
+        .then(response => {
+            console.log('Respuesta exitosa de getOutboundContactlists'); // Agregado
+            console.log('Response:', response); // Agregado
+            const contactLists = response.entities;
+            displayContactLists(contactLists);
+        })
+        .catch(error => {
+            console.error('Error al cargar las contact lists:', error);
+        });
+}
+
 function displayContactLists(contactLists) {
-    console.log('displayContactLists', contactLists);
+    console.log('Inicio de displayContactLists'); // Agregado
+    console.log('ContactLists:', contactLists); // Agregado
+
     const contactListsContainer = document.getElementById('contactLists');
 
     contactLists.forEach(list => {
@@ -7,17 +26,4 @@ function displayContactLists(contactLists) {
         listItem.textContent = `${list.name} - ${list.id}`;
         contactListsContainer.appendChild(listItem);
     });
-}
-
-function fetchContactLists(platformClient) {
-    console.log('fetchContactLists');
-    const apiInstance = new platformClient.OutboundApi();
-
-    apiInstance.getOutboundContactlists()
-        .then(response => {
-            console.log('getOutboundContactlists response', response);
-            const contactLists = response.entities;
-            displayContactLists(contactLists);
-        })
-        .catch(error => console.error('Error al cargar las contact lists:', error));
 }
