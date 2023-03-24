ab09c4f2940c19640f912ad9c3b5d3595ebbd873
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-24 08:04:51 +0100

    Update getContactLists.js

--- getContactLists.js
+++ getContactLists.js
@@ -1,38 +1,30 @@
-function addShowContactListsButtonListener(platformClient) {
-    const showContactListsBtn = document.getElementById('showContactListsBtn');
-
-    showContactListsBtn.addEventListener('click', function () {
-        fetchContactLists(platformClient);
-    });
-}
-
 function fetchContactLists(platformClient) {
     console.log('Starting fetchContactLists');
-    const apiInstance = new platformClient.OutboundApi();
+    
+    function displayContactLists(contactLists) {
+        console.log('displayContactLists', contactLists);
+        const contactListsContainer = document.getElementById('contactLists');
 
-    apiInstance.getOutboundContactlists()
-        .then(response => {
-            console.log('getOutboundContactlists response', response);
-            const contactLists = response.entities;
-            displayContactLists(contactLists);
-        })
-        .catch(error => console.error('Error al cargar las contact lists:', error));
+        contactLists.forEach(list => {
+            const listItem = document.createElement('p');
+            listItem.textContent = `${list.name} - ${list.id}`;
+            contactListsContainer.appendChild(listItem);
+        });
+    }
+
+    function fetchContactListsData() {
+        console.log('fetchContactListsData');
+        const apiInstance = new platformClient.OutboundApi();
+
+        apiInstance.getOutboundContactlists()
+            .then(response => {
+                console.log('getOutboundContactlists response', response);
+                const contactLists = response.entities;
+                displayContactLists(contactLists);
+            })
+            .catch(error => console.error('Error al cargar las contact lists:', error));
+    }
+    
+    console.log('before fetchContactListsData');
+    fetchContactListsData();
 }
-
-function displayContactLists(contactLists) {
-    console.log('displayContactLists', contactLists);
-    const contactListsContainer = document.getElementById('contactLists');
-
-    contactLists.forEach(list => {
-        const listItem = document.createElement('p');
-        listItem.textContent = `${list.name} - ${list.id}`;
-        contactListsContainer.appendChild(listItem);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    const clientId = '479255ae-cf23-4c2e-9209-555370df882c';
-    startGCSDKs(clientId).then(platformClient => {
-        addShowContactListsButtonListener(platformClient);
-    });
-});
