06e905119871561a48690ab299eb8001b0d7aab4
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-24 09:41:02 +0100

    Update getContactLists.js

--- getContactLists.js
+++ getContactLists.js
@@ -1,43 +1,66 @@
-// getContactLists.js
 function fetchContactLists(platformClient) {
-    console.log('getContactLists called');
+  console.log('getContactLists called');
 
-    function displayContactLists(contactLists) {
-        console.log('displayContactLists', contactLists);
-        const contactListsContainer = document.getElementById('contactLists');
+  function displayContactLists(contactLists) {
+    console.log('displayContactLists', contactLists);
+    const contactListsTableBody = document.querySelector('#contactListsTable tbody');
 
-        contactLists.forEach(list => {
-            const listItem = document.createElement('p');
-            listItem.textContent = `${list.name} - ${list.id}`;
-            contactListsContainer.appendChild(listItem);
-        });
-    }
+    contactLists.forEach(list => {
+      const row = document.createElement('tr');
 
-    function fetchContactListsFromApi() {
-        console.log('fetchContactListsFromApi');
-        const apiInstance = new platformClient.OutboundApi();
+      const idCell = document.createElement('td');
+      idCell.textContent = list.id;
+      row.appendChild(idCell);
 
-        apiInstance.getOutboundContactlists()
-            .then(response => {
-                console.log('getOutboundContactlists response', response);
-                const contactLists = response.entities;
-                displayContactLists(contactLists);
-            })
-            .catch(error => console.error('Error al cargar las contact lists:', error));
-    }
+      const nameCell = document.createElement('td');
+      nameCell.textContent = list.name;
+      row.appendChild(nameCell);
 
-    fetchContactListsFromApi();
+      const countCell = document.createElement('td');
+      countCell.textContent = list.size;
+      row.appendChild(countCell);
+
+      const createdByCell = document.createElement('td');
+      createdByCell.textContent = list.createdBy.name;
+      row.appendChild(createdByCell);
+
+      const createdDateCell = document.createElement('td');
+      createdDateCell.textContent = new Date(list.dateCreated).toLocaleString();
+      row.appendChild(createdDateCell);
+
+      const modifiedDateCell = document.createElement('td');
+      modifiedDateCell.textContent = new Date(list.dateModified).toLocaleString();
+      row.appendChild(modifiedDateCell);
+
+      contactListsTableBody.appendChild(row);
+    });
+  }
+
+  function fetchContactListsFromApi() {
+    console.log('fetchContactListsFromApi');
+    const apiInstance = new platformClient.OutboundApi();
+
+    apiInstance.getOutboundContactlists()
+      .then(response => {
+        console.log('getOutboundContactlists response', response);
+        const contactLists = response.entities;
+        displayContactLists(contactLists);
+      })
+      .catch(error => console.error('Error al cargar las contact lists:', error));
+  }
+
+  fetchContactListsFromApi();
 }
 
 function setupContactListButton(clientId) {
-    document.addEventListener('DOMContentLoaded', function () {
-        const showContactListButton = document.getElementById('showContactListButton');
-        if (showContactListButton) {
-            showContactListButton.addEventListener('click', function () {
-                startGCSDKs(clientId).then(platformClient => {
-                    fetchContactLists(platformClient);
-                });
-            });
-        }
-    });
+  document.addEventListener('DOMContentLoaded', function () {
+    const showContactListButton = document.getElementById('showContactLists');
+    if (showContactListButton) {
+      showContactListButton.addEventListener('click', function () {
+        startGCSDKs(clientId).then(platformClient => {
+          fetchContactLists(platformClient);
+        });
+      });
+    }
+  });
 }
