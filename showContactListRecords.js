195b2a5a9e919316a0e2ab55a9c98a4a2e1a73aa
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-25 11:24:03 +0100

    Update showContactListRecords.js

--- showContactListRecords.js
+++ showContactListRecords.js
@@ -1,5 +1,13 @@
-function showContactListRecords(csvUrl) {
+function showContactListRecords(csvData) {
   console.log('showContactListRecords called');
+
+  const lines = csvData.split('\n');
+  const headerLine = lines[0];
+  const headers = headerLine.split(',');
+
+  const rowCount = lines.length - 1;
+  const pageCount = Math.ceil(rowCount / 50);
+  let currentPage = 1;
 
   const table = document.createElement('table');
   table.id = 'contactListRecordsTable';
@@ -17,63 +25,48 @@ function showContactListRecords(csvUrl) {
 
   document.body.appendChild(tableDiv);
 
+  const updateTable = () => {
+    const start = (currentPage - 1) * 50 + 1;
+    const end = Math.min(start + 50, rowCount + 1);
+    const rows = lines.slice(start, end);
+
+    tbody.innerHTML = '';
+    rows.forEach(row => {
+      const fields = row.split(',');
+      const tableRow = document.createElement('tr');
+      fields.forEach(field => {
+        const tableData = document.createElement('td');
+        tableData.textContent = field;
+        tableRow.appendChild(tableData);
+      });
+      tbody.appendChild(tableRow);
+    });
+
+    const previousPageButton = document.createElement('button');
+    previousPageButton.textContent = 'Anterior';
+    previousPageButton.disabled = currentPage === 1;
+    paginationDiv.appendChild(previousPageButton);
+
+    const nextPageButton = document.createElement('button');
+    nextPageButton.textContent = 'Siguiente';
+    nextPageButton.disabled = currentPage === pageCount;
+    paginationDiv.appendChild(nextPageButton);
+
+    previousPageButton.addEventListener('click', () => {
+      currentPage--;
+      updateTable();
+    });
+
+    nextPageButton.addEventListener('click', () => {
+      currentPage++;
+      updateTable();
+    });
+  };
+
+  updateTable();
+
   document.addEventListener('DOMContentLoaded', () => {
     const loadingScreen = document.querySelector('#loadingScreen');
     loadingScreen.style.display = 'none';
   });
-
-  const request = new XMLHttpRequest();
-  request.open('GET', csvUrl, true);
-  request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const lines = request.responseText.split('\n');
-      const headerLine = lines[0];
-      const headers = headerLine.split(',');
-
-      const rowCount = lines.length - 1;
-      const pageCount = Math.ceil(rowCount / 50);
-      let currentPage = 1;
-
-      const updateTable = () => {
-        const start = (currentPage - 1) * 50 + 1;
-        const end = Math.min(start + 50, rowCount + 1);
-        const rows = lines.slice(start, end);
-
-        tbody.innerHTML = '';
-        rows.forEach(row => {
-          const fields = row.split(',');
-          const tableRow = document.createElement('tr');
-          fields.forEach(field => {
-            const tableData = document.createElement('td');
-            tableData.textContent = field;
-            tableRow.appendChild(tableData);
-          });
-          tbody.appendChild(tableRow);
-        });
-
-        const previousPageButton = document.createElement('button');
-        previousPageButton.textContent = 'Anterior';
-        previousPageButton.disabled = currentPage === 1;
-        paginationDiv.appendChild(previousPageButton);
-
-        const nextPageButton = document.createElement('button');
-        nextPageButton.textContent = 'Siguiente';
-        nextPageButton.disabled = currentPage === pageCount;
-        paginationDiv.appendChild(nextPageButton);
-
-        previousPageButton.addEventListener('click', () => {
-          currentPage--;
-          updateTable();
-        });
-
-        nextPageButton.addEventListener('click', () => {
-          currentPage++;
-          updateTable();
-        });
-      };
-
-      updateTable();
-    }
-  };
-  request.send();
 }
