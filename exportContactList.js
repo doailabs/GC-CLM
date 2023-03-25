b84d9405e81e42be025826b00420e8ba8f0a0276
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-25 16:32:06 +0100

    Update exportContactList.js

--- exportContactList.js
+++ exportContactList.js
@@ -21,7 +21,7 @@ function initiateContactListExport(platformClient, contactListId, clientId) {
 function downloadExportedCsv(platformClient, contactListId, jobId, clientId, tries = 0) {
   const apiInstance = new platformClient.OutboundApi();
   let opts = { 
-    "download": "false"
+    "download": "true"
   };
   apiInstance.getOutboundContactlistExport(contactListId, opts)
     .then(response => {
