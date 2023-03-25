167d082973898af9e097a10640556fc644dc6458
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-25 07:45:02 +0100

    Update getContactLists.js

--- getContactLists.js
+++ getContactLists.js
@@ -1,91 +1,94 @@
 let currentPage = 1;
 let platformClientInstance;
+const contactListHandlers = {
+  function fetchContactLists(platformClient, pageNumber = 1) {
+    currentPage = pageNumber;
+    platformClientInstance = platformClient;
+    console.log('getContactLists called');
 
-function fetchContactLists(platformClient, pageNumber = 1) {
-  currentPage = pageNumber;
-  platformClientInstance = platformClient;
-  console.log('getContactLists called');
+    function displayContactLists(contactLists) {
+      console.log('displayContactLists', contactLists);
+      const contactListsTableBody = document.querySelector('#contactListsTable tbody');
+      contactListsTableBody.innerHTML = '';
 
-  function displayContactLists(contactLists) {
-    console.log('displayContactLists', contactLists);
-    const contactListsTableBody = document.querySelector('#contactListsTable tbody');
-    contactListsTableBody.innerHTML = '';
+      contactLists.forEach(list => {
+        const row = document.createElement('tr');
+        const idCell = document.createElement('td');
+        idCell.textContent = list.id;
+        const nameCell = document.createElement('td');
+        nameCell.textContent = list.name;
+        const dateCreatedCell = document.createElement('td');
+        dateCreatedCell.textContent = list.dateCreated;
+        const divisionCell = document.createElement('td');
+        divisionCell.textContent = list.division.name;
+        row.appendChild(idCell);
+        row.appendChild(nameCell);
+        row.appendChild(dateCreatedCell);
+        row.appendChild(divisionCell);
+        contactListsTableBody.appendChild(row);
+      });
+    }
 
-    contactLists.forEach(list => {
-      const row = document.createElement('tr');
-      const idCell = document.createElement('td');
-      idCell.textContent = list.id;
-      const nameCell = document.createElement('td');
-      nameCell.textContent = list.name;
-      const dateCreatedCell = document.createElement('td');
-      dateCreatedCell.textContent = list.dateCreated;
-      const divisionCell = document.createElement('td');
-      divisionCell.textContent = list.division.name;
-      row.appendChild(idCell);
-      row.appendChild(nameCell);
-      row.appendChild(dateCreatedCell);
-      row.appendChild(divisionCell);
-      contactListsTableBody.appendChild(row);
+    function fetchContactListsFromApi(pageNumber) {
+      console.log('fetchContactListsFromApi');
+      const apiInstance = new platformClient.OutboundApi();
+      const pageSize = 25;
+
+      const opts = {
+        "pageSize": pageSize,
+        "pageNumber": pageNumber
+      };
+
+      apiInstance.getOutboundContactlists(opts)
+        .then(response => {
+          console.log('getOutboundContactlists response', response);
+          const contactLists = response.entities;
+          const totalPages = response.pageCount;
+          displayContactLists(contactLists);
+          updatePaginationButtons(totalPages);
+        })
+        .catch(error => console.error('Error al cargar las contact lists:', error));
+    }
+
+
+  function updatePaginationButtons(totalPages) {
+    const previousPageBtn = document.querySelector('#previousPageBtn');
+    const nextPageBtn = document.querySelector('#nextPageBtn');
+
+    if (currentPage === 1) {
+      previousPageBtn.disabled = true;
+    } else {
+      previousPageBtn.disabled = false;
+    }
+
+    if (currentPage === totalPages) {
+      nextPageBtn.disabled = true;
+    } else {
+      nextPageBtn.disabled = false;
+    }
+
+    previousPageBtn.onclick = () => {
+      currentPage -= 1;
+      fetchContactLists(platformClientInstance, currentPage);
+    };
+    nextPageBtn.onclick = () => {
+      currentPage += 1;
+      fetchContactLists(platformClientInstance, currentPage);
+    };
+  }
+
+  addShowContactListsButtonListener(platformClient) {
+    console.log('addShowContactListsButtonListener called');
+
+    const showContactListsButton = document.querySelector('#showContactLists');
+    const contactListsContainer = document.querySelector('#contactLists');
+
+    showContactListsButton.addEventListener('click', () => {
+      contactListsContainer.innerHTML = '';
+      contactListHandlers.fetchContactLists(platformClient);
     });
   }
+};
 
-  function fetchContactListsFromApi(pageNumber) {
-    console.log('fetchContactListsFromApi');
-    const apiInstance = new platformClient.OutboundApi();
-    const pageSize = 25;
-
-    const opts = {
-      "pageSize": pageSize,
-      "pageNumber": pageNumber
-    };
-
-    apiInstance.getOutboundContactlists(opts)
-      .then(response => {
-        console.log('getOutboundContactlists response', response);
-        const contactLists = response.entities;
-        const totalPages = response.pageCount;
-        displayContactLists(contactLists);
-        updatePaginationButtons(totalPages);
-      })
-      .catch(error => console.error('Error al cargar las contact lists:', error));
-  }
-
-
-function updatePaginationButtons(totalPages) {
-  const previousPageBtn = document.querySelector('#previousPageBtn');
-  const nextPageBtn = document.querySelector('#nextPageBtn');
-
-  if (currentPage === 1) {
-    previousPageBtn.disabled = true;
-  } else {
-    previousPageBtn.disabled = false;
-  }
-
-  if (currentPage === totalPages) {
-    nextPageBtn.disabled = true;
-  } else {
-    nextPageBtn.disabled = false;
-  }
-
-  previousPageBtn.onclick = () => {
-    currentPage -= 1;
-    fetchContactLists(platformClientInstance, currentPage);
-  };
-  nextPageBtn.onclick = () => {
-    currentPage += 1;
-    fetchContactLists(platformClientInstance, currentPage);
-  };
-}
-  
-function addShowContactListsButtonListener(platformClient) {
-  console.log('addShowContactListsButtonListener called');
-
-  const showContactListsButton = document.querySelector('#showContactLists');
-  const contactListsContainer = document.querySelector('#contactLists');
-
-  showContactListsButton.addEventListener('click', () => {
-    contactListsContainer.innerHTML = '';
-    fetchContactLists(platformClient);
-  });
-}  
+window.contactListHandlers = contactListHandlers;
 
