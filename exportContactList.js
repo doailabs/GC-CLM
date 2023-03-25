7d3221bbdac57b3233051eb1b99ca7f78b5320b5
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-25 09:27:53 +0100

    Update exportContactList.js

--- exportContactList.js
+++ exportContactList.js
@@ -1,1 +1,55 @@
-a
+let selectedContactListId;
+
+function handleContactListSelection(contactListId) {
+  selectedContactListId = contactListId;
+  initiateContactListExport(contactListId);
+}
+
+function initiateContactListExport(contactListId) {
+  const apiInstance = new platformClient.OutboundApi();
+  apiInstance.postOutboundContactlistExport(contactListId)
+    .then(response => {
+      console.log('Export initiated:', response);
+      waitForExportCompletion(contactListId, response.id);
+    })
+    .catch(error => console.error('Error initiating contact list export:', error));
+}
+
+function waitForExportCompletion(contactListId, jobId) {
+  const apiInstance = new platformClient.OutboundApi();
+  const checkInterval = setInterval(() => {
+    apiInstance.getOutboundContactlistExport(contactListId, jobId)
+      .then(response => {
+        if (response.status === 'completed') {
+          clearInterval(checkInterval);
+          downloadExportedCsv(response.uri);
+        } else if (response.status === 'failed') {
+          clearInterval(checkInterval);
+          console.error('Export job failed');
+        }
+      })
+      .catch(error => {
+        clearInterval(checkInterval);
+        console.error('Error checking export job status:', error);
+      });
+  }, 1000); // Poll the job status every second
+}
+
+function downloadExportedCsv(uri) {
+  fetch(uri)
+    .then(response => response.blob())
+    .then(blob => {
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = `contact_list_${selectedContactListId}.csv`;
+      document.body.appendChild(link);
+      link.click();
+      setTimeout(() => {
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+      }, 100);
+    })
+    .catch(error => console.error('Error downloading exported CSV:', error));
+}
+
