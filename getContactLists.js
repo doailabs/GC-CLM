104dbc734b433f521d117a5c1d1019524be27e38
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-25 09:36:23 +0100

    Update getContactLists.js

--- getContactLists.js
+++ getContactLists.js
@@ -21,23 +21,25 @@ const contactListHandlers = {
         dateCreatedCell.textContent = list.dateCreated;
         const divisionCell = document.createElement('td');
         divisionCell.textContent = list.division.name;
-        const radioCell = document.createElement('td');
+
+        const radioButtonCell = document.createElement('td');
+
         const radioButton = document.createElement('input');
         radioButton.type = 'radio';
-        radioButton.name = 'contactList';
+        radioButton.name = 'contactListSelection';
         radioButton.value = list.id;
-        radioButton.addEventListener('change', () => {
-          handleContactListSelection(list.id);
-        });
-        radioCell.appendChild(radioButton);
-        row.appendChild(radioCell);
+        radioButton.addEventListener('change', () => handleContactListSelection(list.id));
+
+        radioButtonCell.appendChild(radioButton);
         row.appendChild(idCell);
         row.appendChild(nameCell);
         row.appendChild(dateCreatedCell);
         row.appendChild(divisionCell);
+        row.appendChild(radioButtonCell); 
         contactListsTableBody.appendChild(row);
       });
     }
+
 
     function fetchContactListsFromApi(pageNumber) {
       console.log('fetchContactListsFromApi');
