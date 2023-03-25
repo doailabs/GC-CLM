75e5699bf6ed3751e3fb5516254a9b04bd58f051
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-25 10:40:59 +0100

    Update showContactListRecords.js

--- showContactListRecords.js
+++ showContactListRecords.js
@@ -1,88 +1,74 @@
-let csvData = [];
-let currentPage = 1;
-const rowsPerPage = 25
-let csvFilePath = '';
+function showContactListRecords(csvUrl) {
+  console.log('showContactListRecords called');
 
-// Obtener datos del CSV y mostrar la tabla
-fetch(csvFilePath)
-  .then(response => response.text())
-  .then(data => {
-    // Parsear el CSV en un arreglo de objetos
-    csvData = parseCsv(data);
-    // Mostrar la tabla con los primeros 25 registros
-    renderTable(currentPage);
-  })
-  .catch(error => console.error('Error al cargar el archivo CSV:', error));
-
-
-// Función para parsear el CSV en un arreglo de objetos
-function parseCsv(csv) {
-  const lines = csv.trim().split('\n');
-  const headers = lines.shift().split(',');
-  return lines.map(line => {
-    const values = line.split(',');
-    return headers.reduce((obj, header, index) => {
-      obj[header.trim()] = values[index].trim();
-      return obj;
-    }, {});
-  });
-}
-
-// Función para mostrar la tabla con los registros en la página actual
-function renderTable(page) {
-  // Calcular índices de inicio y fin de la página actual
-  const start = (page - 1) * rowsPerPage;
-  const end = start + rowsPerPage;
-  // Obtener los registros de la página actual
-  const pageData = csvData.slice(start, end);
-  // Crear la tabla y agregar los encabezados
   const table = document.createElement('table');
-  const thead = document.createElement('thead');
-  const trHead = document.createElement('tr');
-  Object.keys(pageData[0]).forEach(header => {
-    const th = document.createElement('th');
-    th.textContent = header;
-    trHead.appendChild(th);
-  });
-  thead.appendChild(trHead);
-  table.appendChild(thead);
-  // Agregar las filas de datos a la tabla
-  const tbody = document.createElement('tbody');
-  pageData.forEach(rowData => {
-    const trBody = document.createElement('tr');
-    Object.values(rowData).forEach(value => {
-      const td = document.createElement('td');
-      td.textContent = value;
-      trBody.appendChild(td);
-    });
-    tbody.appendChild(trBody);
-  });
-  table.appendChild(tbody);
-  // Mostrar la tabla en la pantalla
-  const tableContainer = document.querySelector('#table-container');
-  tableContainer.innerHTML = '';
-  tableContainer.appendChild(table);
-  // Mostrar los controles de paginación
-  renderPagination();
-}
+  table.id = 'contactListRecordsTable';
 
-// Función para mostrar los controles de paginación
-function renderPagination() {
-  // Calcular el número total de páginas
-  const totalPages = Math.ceil(csvData.length / rowsPerPage);
-  // Crear los botones de paginación
-  const paginationContainer = document.querySelector('#pagination-container');
-  paginationContainer.innerHTML = '';
-  for (let i = 1; i <= totalPages; i++) {
-    const button = document.createElement('button');
-    button.textContent = i;
-    if (i === currentPage) {
-      button.classList.add('active');
+  const tbody = document.createElement('tbody');
+  table.appendChild(tbody);
+
+  const tableDiv = document.createElement('div');
+  tableDiv.id = 'tableDiv';
+  tableDiv.appendChild(table);
+
+  const paginationDiv = document.createElement('div');
+  paginationDiv.id = 'pagination';
+  tableDiv.appendChild(paginationDiv);
+
+  document.body.appendChild(tableDiv);
+
+  const request = new XMLHttpRequest();
+  request.open('GET', csvUrl, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const lines = request.responseText.split('\n');
+      const headerLine = lines[0];
+      const headers = headerLine.split(',');
+
+      const rowCount = lines.length - 1;
+      const pageCount = Math.ceil(rowCount / 50);
+      let currentPage = 1;
+
+      const updateTable = () => {
+        const start = (currentPage - 1) * 50 + 1;
+        const end = Math.min(start + 50, rowCount + 1);
+        const rows = lines.slice(start, end);
+
+        tbody.innerHTML = '';
+        rows.forEach(row => {
+          const fields = row.split(',');
+          const tableRow = document.createElement('tr');
+          fields.forEach(field => {
+            const tableData = document.createElement('td');
+            tableData.textContent = field;
+            tableRow.appendChild(tableData);
+          });
+          tbody.appendChild(tableRow);
+        });
+
+        const previousPageButton = document.createElement('button');
+        previousPageButton.textContent = 'Anterior';
+        previousPageButton.disabled = currentPage === 1;
+        paginationDiv.appendChild(previousPageButton);
+
+        const nextPageButton = document.createElement('button');
+        nextPageButton.textContent = 'Siguiente';
+        nextPageButton.disabled = currentPage === pageCount;
+        paginationDiv.appendChild(nextPageButton);
+
+        previousPageButton.addEventListener('click', () => {
+          currentPage--;
+          updateTable();
+        });
+
+        nextPageButton.addEventListener('click', () => {
+          currentPage++;
+          updateTable();
+        });
+      };
+
+      updateTable();
     }
-    button.addEventListener('click', () => {
-      currentPage = i;
-      renderTable(currentPage);
-    });
-    paginationContainer.appendChild(button);
-  }
+  };
+  request.send();
 }
