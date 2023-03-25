2d78abb407b20e7a7c80e916bb90c1a9e9540ec8
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-25 07:38:49 +0100

    Update getContactLists.js

--- getContactLists.js
+++ getContactLists.js
@@ -76,4 +76,16 @@ function updatePaginationButtons(totalPages) {
     fetchContactLists(platformClientInstance, currentPage);
   };
 }
+  
+function addShowContactListsButtonListener(platformClient) {
+  console.log('addShowContactListsButtonListener called');
+
+  const showContactListsButton = document.querySelector('#showContactLists');
+  const contactListsContainer = document.querySelector('#contactLists');
+
+  showContactListsButton.addEventListener('click', () => {
+    contactListsContainer.innerHTML = '';
+    fetchContactLists(platformClient);
+  });
+}  
 
