3559deb85c67926a3c82d74dac2e7c10e1285780
doailabs <128745776+doailabs@users.noreply.github.com>
2023-03-27 16:35:57 +0200

    Update exportContactList.js

--- exportContactList.js
+++ exportContactList.js
@@ -20,19 +20,28 @@ async function initiateContactListExport(platformClient, contactListId) {
 
 async function downloadExportedCsv(platformClient, contactListId, tries = 0) {
   const apiInstance = new platformClient.OutboundApi();
-  let downloadUrl = '';
   try {
-    const response = await apiInstance.getOutboundContactlistExport(contactListId, {"download": "false"});
+    const response = await apiInstance.getOutboundContactlistExport(contactListId, {download: 'false'});
     console.log('Estado de exportación:', response.status);
     if (response.status === 'COMPLETE') {
-      downloadUrl = response.uri;
+      const downloadUrl = response.uri + '?issueRedirect=false';
+      console.log('URL de descarga:', downloadUrl);
+      const response2 = await fetch(downloadUrl, {
+        headers: {
+          Authorization: `Bearer ${platformClient.getAuth().getAccessToken()}`
+        }
+      });
+      const responseBody = await response2.json();
+      console.log('URL del archivo de exportación:', responseBody.uri);
+      csvData = responseBody.uri;
+      showContactListRecords(csvData);
     } else {
       console.log('Esperando que se complete la exportación...');
       await new Promise(resolve => setTimeout(resolve, 5000));
       return downloadExportedCsv(platformClient, contactListId, tries + 1);
     }
   } catch (error) {
-    console.error('Error al comprobar el estado de la exportación:', error);
+    console.error('Error al descargar el archivo CSV:', error);
     if (tries < 3) {
       console.log(`Reintentando en ${tries + 1} segundos...`);
       await new Promise(resolve => setTimeout(resolve, (tries + 1) * 1000));
@@ -40,31 +49,5 @@ async function downloadExportedCsv(platformClient, contactListId, tries = 0) {
     } else {
       throw error;
     }
-  }
-
-  try {
-    const redirectResponse = await fetch(`${downloadUrl}?issueRedirect=false`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${platformClient.getAuth().getAccessToken()}`
-      },
-      redirect: 'manual'
-    });
-    console.log('Respuesta de redirección:', redirectResponse);
-    if (redirectResponse.status === 307) {
-      const redirectUrl = redirectResponse.headers.get('Location');
-      console.log('URL de redirección:', redirectUrl);
-      const response = await fetch(redirectUrl);
-      const responseBody = await response.json();
-      console.log('URL del archivo de exportación:', responseBody.url);
-      csvData = responseBody.url;
-      showContactListRecords(csvData);
-    } else {
-      console.error('Error al obtener la URL de redirección:', redirectResponse);
-      throw new Error('Error al obtener la URL de redirección');
-    }
-  } catch (error) {
-    console.error('Error al descargar el archivo CSV:', error);
-    throw error;
   }
 }
